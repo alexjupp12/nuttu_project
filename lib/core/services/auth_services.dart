b66dbbf4c6fd0c2870176953cf9ce1fba0e59840
Alex Lemus <lemuscoloradoalexhumberto@gmail.com>
2025-09-30 00:57:22 -0500

    Creamos el documento el cual usaremos para la autenticación del usuario. usando la API nueva para la APP

---
+++ lib/core/services/auth_services.dart
@@ -0,0 +1,45 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class AuthService {
+  // URL base del servidor muy importante para realizar la conexión
+  final String _baseUrl =
+      'http://10.0.2.2:3000'; // Cambia esto por la URL de tu servidor
+  //--------------------------------------------------
+
+  // Función para registrar un nuevo usuario
+  Future<bool> register(String email, String password) async {
+    final url = Uri.parse('$_baseUrl/register');
+    try {
+      final response = await http.post(
+        url,
+        headers: {'Content-Type': 'application/json'},
+        body: json.encode({'email': email, 'password': password}),
+      );
+
+      // 201 es "ok creado"
+      return response.statusCode == 201;
+    } catch (e) {
+      print(e);
+      return false;
+    }
+  }
+
+  //Método para uniciar sesión
+  Future<bool> login(String email, String password) async {
+    final url = Uri.parse('$_baseUrl/login');
+    try {
+      final response = await http.post(
+        url,
+        headers: {'Content-Type': 'application/json'},
+        body: json.encode({'email': email, 'password': password}),
+      );
+
+      // 200 es "ok"
+      return response.statusCode == 200;
+    } catch (e) {
+      print(e);
+      return false;
+    }
+  }
+}
